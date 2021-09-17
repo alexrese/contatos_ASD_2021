35d3cffcaa11c322671bfc5bcc32c8136017ce85
Alex Luciano Roesler Rese <alexrese@outlook.com>
2021-09-16 23:13:38 -0300

    lista de contatos com pesquisar

---
+++ Contato/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace Contato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgContato = new System.Windows.Forms.DataGridView();
+            this.ColumnID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnTelefone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnRedeSocial = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnAtivo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbTelefone = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbRedeSocial = new System.Windows.Forms.TextBox();
+            this.btAdicionar = new System.Windows.Forms.Button();
+            this.btAlterar = new System.Windows.Forms.Button();
+            this.btDeletar = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbPesquisarID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgContato)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgContato
+            // 
+            this.dgContato.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgContato.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnID,
+            this.ColumnNome,
+            this.ColumnTelefone,
+            this.ColumnRedeSocial,
+            this.ColumnAtivo});
+            this.dgContato.Location = new System.Drawing.Point(27, 140);
+            this.dgContato.Name = "dgContato";
+            this.dgContato.Size = new System.Drawing.Size(723, 251);
+            this.dgContato.TabIndex = 0;
+            this.dgContato.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgContato_CellContentDoubleClick);
+            // 
+            // ColumnID
+            // 
+            this.ColumnID.HeaderText = "ID";
+            this.ColumnID.Name = "ColumnID";
+            // 
+            // ColumnNome
+            // 
+            this.ColumnNome.HeaderText = "Nome";
+            this.ColumnNome.Name = "ColumnNome";
+            this.ColumnNome.Width = 200;
+            // 
+            // ColumnTelefone
+            // 
+            this.ColumnTelefone.HeaderText = "Telefone";
+            this.ColumnTelefone.Name = "ColumnTelefone";
+            // 
+            // ColumnRedeSocial
+            // 
+            this.ColumnRedeSocial.HeaderText = "Rede Social";
+            this.ColumnRedeSocial.Name = "ColumnRedeSocial";
+            // 
+            // ColumnAtivo
+            // 
+            this.ColumnAtivo.HeaderText = "Ativo";
+            this.ColumnAtivo.Name = "ColumnAtivo";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLimpar.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(27, 106);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(124, 28);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // tbID
+            // 
+            this.tbID.Enabled = false;
+            this.tbID.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbID.Location = new System.Drawing.Point(59, 33);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(40, 26);
+            this.tbID.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(114, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Name:";
+            // 
+            // tbNome
+            // 
+            this.tbNome.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNome.Location = new System.Drawing.Point(179, 33);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(299, 26);
+            this.tbNome.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(484, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Telefone:";
+            // 
+            // tbTelefone
+            // 
+            this.tbTelefone.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTelefone.Location = new System.Drawing.Point(572, 33);
+            this.tbTelefone.Name = "tbTelefone";
+            this.tbTelefone.Size = new System.Drawing.Size(178, 26);
+            this.tbTelefone.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Rede Social";
+            // 
+            // tbRedeSocial
+            // 
+            this.tbRedeSocial.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbRedeSocial.Location = new System.Drawing.Point(132, 74);
+            this.tbRedeSocial.Name = "tbRedeSocial";
+            this.tbRedeSocial.Size = new System.Drawing.Size(618, 26);
+            this.tbRedeSocial.TabIndex = 8;
+            // 
+            // btAdicionar
+            // 
+            this.btAdicionar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btAdicionar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btAdicionar.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAdicionar.Location = new System.Drawing.Point(157, 106);
+            this.btAdicionar.Name = "btAdicionar";
+            this.btAdicionar.Size = new System.Drawing.Size(124, 28);
+            this.btAdicionar.TabIndex = 10;
+            this.btAdicionar.Text = "ADICIONAR";
+            this.btAdicionar.UseVisualStyleBackColor = false;
+            this.btAdicionar.Click += new System.EventHandler(this.btAdicionar_Click);
+            // 
+            // btAlterar
+            // 
+            this.btAlterar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btAlterar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btAlterar.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAlterar.Location = new System.Drawing.Point(287, 106);
+            this.btAlterar.Name = "btAlterar";
+            this.btAlterar.Size = new System.Drawing.Size(124, 28);
+            this.btAlterar.TabIndex = 11;
+            this.btAlterar.Text = "ALTERAR";
+            this.btAlterar.UseVisualStyleBackColor = false;
+            this.btAlterar.Click += new System.EventHandler(this.btAlterar_Click);
+            // 
+            // btDeletar
+            // 
+            this.btDeletar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btDeletar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btDeletar.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDeletar.Location = new System.Drawing.Point(417, 106);
+            this.btDeletar.Name = "btDeletar";
+            this.btDeletar.Size = new System.Drawing.Size(124, 28);
+            this.btDeletar.TabIndex = 12;
+            this.btDeletar.Text = "DELETAR";
+            this.btDeletar.UseVisualStyleBackColor = false;
+            this.btDeletar.Click += new System.EventHandler(this.btDeletar_Click);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnPesquisar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnPesquisar.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.Location = new System.Drawing.Point(547, 106);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(124, 28);
+            this.btnPesquisar.TabIndex = 13;
+            this.btnPesquisar.Text = "PESQUISAR";
+            this.btnPesquisar.UseVisualStyleBackColor = false;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(675, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 18);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "ID:";
+            // 
+            // tbPesquisarID
+            // 
+            this.tbPesquisarID.Font = new System.Drawing.Font("Quicksand Book", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPesquisarID.Location = new System.Drawing.Point(710, 108);
+            this.tbPesquisarID.Name = "tbPesquisarID";
+            this.tbPesquisarID.Size = new System.Drawing.Size(40, 26);
+            this.tbPesquisarID.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(781, 403);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbPesquisarID);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.btDeletar);
+            this.Controls.Add(this.btAlterar);
+            this.Controls.Add(this.btAdicionar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbRedeSocial);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbTelefone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.dgContato);
+            this.Name = "Form1";
+            this.Text = "Contato";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgContato)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgContato;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTelefone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnRedeSocial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnAtivo;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbTelefone;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbRedeSocial;
+        private System.Windows.Forms.Button btAdicionar;
+        private System.Windows.Forms.Button btAlterar;
+        private System.Windows.Forms.Button btDeletar;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbPesquisarID;
+    }
+}
+
---
+++ Contato/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Contato
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "contato";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            conexaoBD.SslMode = 0;
+            return conexaoBD;
+        }
+
+        private void atualizarGrid()
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM pessoa";
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dgContato.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dgContato.Rows[0].Clone();//FAZ UM CAST E CLONA A LINHA DA TABELA
+                    row.Cells[0].Value = reader.GetInt32(0);//ID
+                    row.Cells[1].Value = reader.GetString(1);//NOME
+                    row.Cells[2].Value = reader.GetString(2);//TELEFONE
+                    row.Cells[3].Value = reader.GetString(3);//REDESOCIAL
+                    row.Cells[4].Value = reader.GetString(4);//ATIVO
+                    dgContato.Rows.Add(row);//ADICIONO A LINHA NA TABELA
+                }
+
+                realizaConexacoBD.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void limparCampos()
+        {
+            tbID.Clear();
+            tbNome.Clear();
+            tbTelefone.Clear();
+            tbRedeSocial.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            atualizarGrid();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+
+        private void btAdicionar_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "INSERT INTO pessoa (nomePessoa,telefonePessoa,redeSocialPessoa) " +
+                    "VALUES('" + tbNome.Text + "', '" + tbTelefone.Text + "','" + tbRedeSocial.Text + "')";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close();
+                MessageBox.Show("Parabéns\nInserido com sucesso");
+                atualizarGrid();
+                limparCampos();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void dgContato_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgContato.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                dgContato.CurrentRow.Selected = true;
+                //preenche os textbox com as células da linha selecionada
+                tbNome.Text = dgContato.Rows[e.RowIndex].Cells["ColumnNome"].FormattedValue.ToString();
+                tbTelefone.Text = dgContato.Rows[e.RowIndex].Cells["ColumnTelefone"].FormattedValue.ToString();
+                tbRedeSocial.Text = dgContato.Rows[e.RowIndex].Cells["ColumnRedeSocial"].FormattedValue.ToString();
+                tbID.Text = dgContato.Rows[e.RowIndex].Cells["ColumnID"].FormattedValue.ToString();
+            }
+        }
+
+        private void btAlterar_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open(); //Abre a conexão com o banco
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); //Crio um comando SQL
+                comandoMySql.CommandText = "UPDATE pessoa SET nomePessoa= '" + tbNome.Text + "', " +
+                    "telefonePessoa = '" + tbTelefone.Text + "', " +
+                    "redeSocialPessoa = '" + tbRedeSocial.Text + "' " +
+                    " WHERE idPessoa = " + tbID.Text + "";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close(); // Fecho a conexão com o banco
+                MessageBox.Show("Sucesso\nAtualizado com sucesso"); //Exibo mensagem de aviso
+                atualizarGrid();
+                limparCampos();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Não foi possivel abrir a conexão! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btDeletar_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open(); //Abre a conexão com o banco
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); //Crio um comando SQL
+                // "DELETE FROM filme WHERE idFilme = "+ textBoxId.Text +""
+                comandoMySql.CommandText = "UPDATE pessoa SET ativoPessoa = 0 WHERE idPessoa = " + tbID.Text + "";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close(); // Fecho a conexão com o banco
+                MessageBox.Show("Uhlll!\nDeletado com sucesso"); //Exibo mensagem de aviso
+                atualizarGrid();
+                limparCampos();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Não foi possivel abrir a conexão! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private int verificaSeExiste()
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+
+                comandoMySql.CommandText = "SELECT * FROM pessoa WHERE idPessoa = " + Convert.ToInt32(tbPesquisarID.Text);
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dgContato.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    realizaConexacoBD.Close();
+                    return 1;
+
+                }
+
+                realizaConexacoBD.Close();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            int aux = verificaSeExiste();
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+
+                comandoMySql.CommandText = "SELECT * FROM pessoa WHERE idPessoa = " + Convert.ToInt32(tbPesquisarID.Text);
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dgContato.Rows.Clear();
+                if (aux == 1)
+                {
+                    while (reader.Read())
+                    {
+                        DataGridViewRow row = (DataGridViewRow)dgContato.Rows[0].Clone();//FAZ UM CAST E CLONA A LINHA DA TABELA
+                        row.Cells[0].Value = reader.GetInt32(0);//ID
+                        row.Cells[1].Value = reader.GetString(1);//NOME
+                        row.Cells[2].Value = reader.GetString(2);//TELEFONE
+                        row.Cells[3].Value = reader.GetString(3);//REDESOCIAL
+                        row.Cells[4].Value = reader.GetString(4);//ATIVO
+                        dgContato.Rows.Add(row);//ADICIONO A LINHA NA TABELA
+                        MessageBox.Show("Id encontrado");
+                    }
+                }
+                else {
+                    MessageBox.Show("Id não encontrado");
+                }
+
+                
+                realizaConexacoBD.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
